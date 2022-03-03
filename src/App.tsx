64f406e9416db3825630d379649c6eaa6374f57f
jtrenaud1s <jtrenaud1s@gmail.com>
2022-03-03 01:11:50 -0600

    fix for github pages spa

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import EncodedMaterialView from "./views/EncodedMaterialView";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/EDMats">
         <Routes>
           <Route path="/" element={<HomeView />}/>
           <Route path="/raw" element={<RawMaterialView />} />
