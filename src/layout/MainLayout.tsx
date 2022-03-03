b7afad095b9cd755da6efb23e23b957b93b39a10
jtrenaud1s <jtrenaud1s@gmail.com>
2022-03-03 01:35:37 -0600

    add footer

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "../layout/NavBar";
 
 import { Container } from "react-bootstrap";
+import Footer from "./Footer";
 
 const MainLayout: React.FC = ({ children }) => {
   return (
     <>
       <NavBar />
       <Container className="mt-3">{children}</Container>
+      <Footer />
     </>
   );
 };
