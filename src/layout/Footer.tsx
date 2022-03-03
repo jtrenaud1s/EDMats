e7ebb0cf3a91e67cf2b9d32e6832557cd3688b59
jtrenaud1s <jtrenaud1s@gmail.com>
2022-03-03 01:37:12 -0600

    footer margin top

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="text-center py-4 w-100 border-top">
+    <div className="text-center py-4 mt-4 w-100 border-top">
       <small>
         Original Guide by{" "}
         <a href="https://www.youtube.com/watch?v=iHl-SsKQfe4" target="_blank" rel="noreferrer">Hawkes Gaming</a>
